f94ff0ffaa06f12ece5a1021195cd19d09fcb49a
Paul K <pa179kin@gmail.com>
2025-09-12 01:02:14 +0200

    update file hasher

--- marker-api/src/main/kotlin/fyi/pauli/marker/caching/internal/FileHasher.kt
+++ marker-api/src/main/kotlin/fyi/pauli/marker/caching/internal/FileHasher.kt
@@ -1,18 +1,22 @@
 package fyi.pauli.marker.caching.internal
 
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 import java.security.MessageDigest
-import kotlin.io.path.inputStream
 
 internal fun Path.calculateSHA1Hash(): ByteArray {
     val digest = MessageDigest.getInstance("SHA-1")
-    this.inputStream().use { inputStream ->
-        var n = 0
-        val buffer = ByteArray(8192)
-        while (n != -1) {
-            n = inputStream.read(buffer)
-            if (n > 0) digest.update(buffer, 0, n)
+
+    FileChannel.open(this, StandardOpenOption.READ).use { channel ->
+        val buffer = ByteBuffer.allocate(8192)
+        while (channel.read(buffer) != -1) {
+            buffer.flip()
+            digest.update(buffer)
+            buffer.clear()
         }
+
         return digest.digest()
     }
 }
